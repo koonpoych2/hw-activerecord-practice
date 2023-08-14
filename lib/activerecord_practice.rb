0209cee9ef994deb9e4bebece6c51f43220b5b1b
Anecha Khumjun <68006281+koonpoych2@users.noreply.github.com>
2023-08-14 02:54:53 +0000

    Finished

--- lib/activerecord_practice.rb
+++ lib/activerecord_practice.rb
@@ -12,16 +12,59 @@ class Customer < ActiveRecord::Base
   def to_s
     "  [#{id}] #{first} #{last}, <#{email}>, #{birthdate.strftime('%Y-%m-%d')}"
   end
-
+  #
   #  NOTE: Every one of these can be solved entirely by ActiveRecord calls.
   #  You should NOT need to call Ruby library functions for sorting, filtering, etc.
-
   def self.any_candice
     # YOUR CODE HERE to return all customer(s) whose first name is Candice
     # probably something like:  Customer.where(....)
+    Customer.where(first: 'Candice')
+
   end
+
   def self.with_valid_email
     # YOUR CODE HERE to return only customers with valid email addresses (containing '@')
+    Customer.where("email LIKE ? ",'%@%' )
+  end
+
+  def self.with_dot_org_email
+    Customer.where("email LIKE ? ",'%.org' )
+  end
+
+  def self.with_invalid_email
+    Customer.where("email NOT LIKE ? ",'%@%' )
+  end
+
+  def self.with_blank_email
+    Customer.where("email is null")
+  end
+  def self.born_before_1980
+    Customer.where("birthdate < ? ",'1980-01-01')
+  end
+  def self.with_valid_email_and_born_before_1980
+    valid_email = Customer.where("email LIKE ? ",'%@%' )
+    valid_email.where("birthdate < ? ",'1980-01-01')
+  end
+
+  def self.last_names_starting_with_b
+    Customer.where("last LIKE ? ",'B%' ).order("birthdate")
+
+  end
+  def self.twenty_youngest
+    Customer.order("birthdate DESC").limit(20)
+  end
+  def self.update_gussie_murray_birthdate
+    time = Time.parse("2004/2/8")
+    Customer.where(first: 'Gussie').update(birthdate: time)
+  end
+  def self.change_all_invalid_emails_to_blank
+    Customer.where("email NOT LIKE ? ",'%@%' ).update(email: '')
+  end
+  def self.delete_meggie_herman
+    Customer.where(first: "Meggie", last: "Herman").delete_all
+  end
+  def self.delete_everyone_born_before_1978
+    Customer.where("birthdate < ? ",'1978-01-01').delete_all
   end
   # etc. - see README.md for more details
 end
